613c855e1f4866a87b3d3fe513cc1321de872288
JuankoGmzk <37718099+JuankoGmzk@users.noreply.github.com>
2021-04-06 16:29:25 -0500

    First comit

---
+++ Poli-Blogrs/src/main/java/co/edu/poli/blogrs/PoliBlogrsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.poli.blogrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliBlogrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliBlogrsApplication.class, args);
+	}
+
+}
